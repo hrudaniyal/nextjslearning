6f4924cc09fc7394766402c501bd24eb14313f04
hrudaniyal <niyaljohn@gmail.com>
2024-11-17 06:38:50 +0530

    chanage ts to js

--- app/users/[slug]/page.jsx
+++ app/users/[slug]/page.jsx
@@ -1,9 +1,10 @@
-export default async function page({params}:any) {
-  const {slug}= await params;
+export default async function page({ params }) {
+  const { slug } = await params;
   const res = await fetch(`http://localhost:3001/api/${slug}`);
-  const user = await res.json()
+  const user = await res.json();
   return (
-    <section>{user.map((item : any) => {
+    <section>
+      {user.map((item) => {
         const { id, name, email } = item;
         return (
           <div key={id}>
